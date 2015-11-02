77b4ecef67901774dac1dc82921960a7c215aa0f
Dan <DBaldino12@users.noreply.github.com>
2015-11-02 14:27:40 -0500

    Create GreenModule.php

---
+++ Module/Model/GreenModule.php
@@ -0,0 +1,10 @@
+<?php
+class Pricelizer_Module_Model_Greenmodule extends Mage_Core_Model_Abstract
+{
+    public function _construct()
+    {
+        $this->_init('greenmodule/greenmodule');
+    }
+
+}
+?>
